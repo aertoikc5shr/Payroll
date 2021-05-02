fce8bfbb7313733b15b2dd74966a9778a83ef0fd
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 15:29:23 +0800

    让类AddSalariedEmployeeTransaction调用超类的构造函数

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -10,9 +10,7 @@ public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction imple
 	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
-		this.empId = empId;
-		this.name = name;
-		this.address = address;
+		super(empId, name, address);
 		this.salary = salary;
 		// TODO Auto-generated constructor stub
 	}
