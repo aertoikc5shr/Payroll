28227c1c4ca9bbfb7be633b2135baabe49956e4b
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 13:09:46 +0800

    构造AddHourlyEmployeeTransaction函数

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,4 +4,8 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		// TODO Auto-generated constructor stub
+	}
+
 }
