26957516072ecd829b1aeb172d7f939649091f9e
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 15:56:32 +0800

    赋值给字段

--- src/payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -5,8 +5,19 @@ import payroll.Transaction;
 
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
+	private int empId2;
+	private String name2;
+	private String address2;
+	private double salary;
+	private double commissionRate;
+
 	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
 			double commissionRate) {
+				empId2 = empId;
+				name2 = name;
+				address2 = address;
+				this.salary = salary;
+				this.commissionRate = commissionRate;
 		// TODO Auto-generated constructor stub
 	}
 
