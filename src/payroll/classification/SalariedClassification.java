365df353cd720be44ef77f67f19c099cc7f5e203
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 14:38:02 +0800

    添加SalariedClassification构造函数

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -4,9 +4,16 @@ import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification{
 
+	private double salary;
+
+	public SalariedClassification(double salary) {
+		this.salary = salary;
+		// TODO Auto-generated constructor stub
+	}
+
 	public double getSalary() {
 		// TODO Auto-generated method stub
-		return 0;
+		return salary;
 	}
 
 }
