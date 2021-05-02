92f193efa1c3e0cf8d4d399293c1f4c4856528c3
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 15:54:14 +0800

    创建testAddCommissionedEmployee测试

--- src/payroll/test/AddEmployeeTests.java
+++ src/payroll/test/AddEmployeeTests.java
@@ -9,9 +9,11 @@ import payroll.PaymentClassification;
 import payroll.PaymentMethod;
 import payroll.PayrollDatabase;
 import payroll.Transaction;
+import payroll.classification.CommissionedClassification;
 import payroll.classification.HourlyClassification;
 import payroll.classification.SalariedClassification;
 import payroll.method.HoldMethod;
+import payroll.trans.AddCommissionedEmployeeTransaction;
 import payroll.trans.AddHourlyEmployeeTransaction;
 import payroll.trans.AddSalariedEmployeeTransaction;
 
@@ -59,6 +61,29 @@ public class AddEmployeeTests {
 		assertEquals(salary, sc.getSalary(), 0.01);
 		PaymentMethod pm = e.getPaymentMethod();
 		assertTrue(pm instanceof HoldMethod);
+	}
+	
+	@Test
+	public void testAddCommissionedEmployee() {
+		int empId = 1002;
+		String name = "Bill";
+		String address = "Home";
+		double salary = 2410.0;
+		double commissionRate = 0.02;
+		
+		Transaction t = new AddCommissionedEmployeeTransaction(empId, name, address,
+				salary, commissionRate);
+		t.execute();
+		
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNotNull(e);
+		assertEquals(name, e.getName());
+		assertEquals(address, e.getAddress());
+		PaymentClassification pc = e.getPaymentClassification();
+		assertTrue(pc instanceof CommissionedClassification);
+		CommissionedClassification sc = (CommissionedClassification) pc;
+		assertEquals(salary, sc.getSalary(), 0.01);
+		assertEquals(commissionRate, sc.getCommissionRate(), 0.0001);
 	}
 
 }
