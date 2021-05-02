6a8e1bf07a96a6e77d6ad0d52e1f398e78762fa9
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 15:24:54 +0800

    抽取类构建AddEmployeeTransaction共同超类

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -1,17 +1,11 @@
 package payroll.trans;
 
-import payroll.Employee;
 import payroll.PaymentClassification;
-import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.HourlyClassification;
-import payroll.method.HoldMethod;
 
-public class AddHourlyEmployeeTransaction implements Transaction {
+public class AddHourlyEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double hourlyRate;
 
 	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
@@ -23,14 +17,6 @@ public class AddHourlyEmployeeTransaction implements Transaction {
 	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		Employee employee=new Employee(empId,name,address);
-		employee.setPaymentClassification(getPaymentClassification());
-		employee.setPaymentMethod(new HoldMethod());
-		PayrollDatabase.save(employee);
-	}
-
 	protected PaymentClassification getPaymentClassification() {
 		return new HourlyClassification(hourlyRate);
 	}
--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,18 +1,12 @@
 package payroll.trans;
 
-import payroll.Employee;
 import payroll.PaymentClassification;
-import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.HourlyClassification;
 import payroll.classification.SalariedClassification;
-import payroll.method.HoldMethod;
 
-public class AddSalariedEmployeeTransaction implements Transaction {
+public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
@@ -21,16 +15,6 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 		this.address = address;
 		this.salary = salary;
 		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		Employee employee=new Employee(empId,name,address);
-		employee.setPaymentClassification(getPaymentClassification());
-		employee.setPaymentMethod(new HoldMethod());
-		PayrollDatabase.save(employee);
-
 	}
 
 	protected PaymentClassification getPaymentClassification() {
