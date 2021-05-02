0d757c6c786a0565e7a232f1f5f1242d0d509666
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 13:12:59 +0800

    将各个构造函数的参数赋值给新的字段

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -4,7 +4,16 @@ import payroll.Transaction;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
 
+	private int empId;
+	private String name;
+	private String address;
+	private double hourlyRate;
+
 	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
 		// TODO Auto-generated constructor stub
 	}
 
