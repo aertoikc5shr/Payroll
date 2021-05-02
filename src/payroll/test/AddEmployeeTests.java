c8c89b1ba24cde01058d6f7d74bd47edaf3982a1
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 13:02:21 +0800

    小时工基本信息

--- src/payroll/test/AddEmployeeTests.java
+++ src/payroll/test/AddEmployeeTests.java
@@ -4,11 +4,37 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import payroll.Employee;
+import payroll.PaymentClassification;
+import payroll.PaymentMethod;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.classification.HourlyClassification;
+import payroll.method.HoldMethod;
+import payroll.trans.AddHourlyEmployeeTransaction;
+
 public class AddEmployeeTests {
 
 	@Test
 	public void testAddHourlyEmployeeTransaction(){
-		
+		int empId = 1001;
+		String name = "Bill";
+		String address = "Home";
+		double hourlyRate = 12.5;
+		// Actions
+		Transaction t = new AddHourlyEmployeeTransaction(empId, name, address, hourlyRate);
+		t.execute();
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNotNull(e);
+		assertEquals(empId, e.getEmpId());
+		assertEquals(name, e.getName());
+		assertEquals(address, e.getAddress());
+		PaymentClassification pc = e.getPaymentClassification();
+		assertTrue(pc instanceof HourlyClassification);
+		HourlyClassification hc = (HourlyClassification) pc;
+		assertEquals(hourlyRate, hc.getHourlyRate(), 0.01);
+		PaymentMethod pm = e.getPaymentMethod();
+		assertTrue(pm instanceof HoldMethod);
 	}
 
 }
