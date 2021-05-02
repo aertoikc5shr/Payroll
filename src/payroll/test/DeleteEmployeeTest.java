5dbb1057d217f162873ef6b12df11a377b41a481
aertoikc5shr <a1317563725032796@163.com>
2021-05-02 16:28:58 +0800

    添加测试代码

--- src/payroll/test/DeleteEmployeeTest.java
+++ src/payroll/test/DeleteEmployeeTest.java
@@ -4,11 +4,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.trans.AddHourlyEmployeeTransaction;
+import payroll.trans.DeleteEmployeeTransaction;
+
 public class DeleteEmployeeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDeleteEmployeeExists() {
+		int empId = 2001;
+		new AddHourlyEmployeeTransaction(empId, "Bill", "Home", 12.5).execute();
+
+		Transaction t = new DeleteEmployeeTransaction(empId);
+		t.execute();
+
+		Employee e = PayrollDatabase.getEmployee(empId);
+		assertNull(e);
 	}
 
 }
