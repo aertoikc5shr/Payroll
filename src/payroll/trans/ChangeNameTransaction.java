0e37ef9767482b337adf05ba543814e8be1d87d1
aertoikc5shr <a1317563725032796@163.com>
2021-05-07 11:43:27 +0800

    创建ChangeNameTransaction类

---
+++ src/payroll/trans/ChangeNameTransaction.java
@@ -0,0 +1,13 @@
+package payroll.trans;
+
+import payroll.Transaction;
+
+public class ChangeNameTransaction implements Transaction {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
