20f8f48ff39350f83b38f889ded9066b3e4a16b7
aertoikc5shr <a1317563725032796@163.com>
2021-05-07 11:45:37 +0800

    赋值给字段

--- src/payroll/trans/ChangeNameTransaction.java
+++ src/payroll/trans/ChangeNameTransaction.java
@@ -4,7 +4,12 @@ import payroll.Transaction;
 
 public class ChangeNameTransaction implements Transaction {
 
+	private int empId;
+	private String newName;
+
 	public ChangeNameTransaction(int empId, String newName) {
+		this.empId = empId;
+		this.newName = newName;
 		// TODO Auto-generated constructor stub
 	}
 
