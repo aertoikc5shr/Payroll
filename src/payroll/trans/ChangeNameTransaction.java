4e2beb3f7573a571523976c286d3f35db78c2c53
aertoikc5shr <a1317563725032796@163.com>
2021-05-07 11:44:59 +0800

    创建ChangeNameTransaction构造方法

--- src/payroll/trans/ChangeNameTransaction.java
+++ src/payroll/trans/ChangeNameTransaction.java
@@ -4,6 +4,10 @@ import payroll.Transaction;
 
 public class ChangeNameTransaction implements Transaction {
 
+	public ChangeNameTransaction(int empId, String newName) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
